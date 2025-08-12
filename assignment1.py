4883f2b27fdb76d34c2da05a1a0ae8e747233f3e
antoPYrobo <antonyrobinaluva@gmail.com>
2025-08-12 10:56:40 +0530

    complete

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+f = int(input("Enter the first number: "))
+
+s = int(input("Enter the second number: "))
+
+print("Addition: ", f+s)
+print("Subtraction: ", f-s)
+print("Multiplication: ", f*s)
+print("Division: ", f/s)
+
+fn = input("Enter your first name: ")
+ln = input("Enter your last name: ")
+print("Hello, "+fn+" "+ln+"! Welcome to the Python program.")
